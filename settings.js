4f8dba092806448568dfdbc4c39478e40a7d227f
VAIBHAV KUMAWAT <97437528+vaibhavk10@users.noreply.github.com>
2024-09-16 17:57:09 +0530

    fixed minor bugs

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'My Bot',
   botName: "Knight Bot",
   botOwner: 'Professor',
-  ownerNumber: '14703105589',
+  ownerNumber: '917023951514',
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",

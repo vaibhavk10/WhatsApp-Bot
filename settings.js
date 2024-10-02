385d697539e78f565c0c76289c451b86357bbffa
Vaibhav Kumawat <97437528+vaibhavk10@users.noreply.github.com>
2024-10-02 19:06:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'My Bot',
   botName: "Knight Bot",
   botOwner: 'Professor',
-  ownerNumber: '917023951514',
+  ownerNumber: '91xxxxxxxxx', //Your phone number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
